c30045efd9f0e99efe1ef36dbfe49a78bfa4b784
lairjr <lairjr@outlook.com>
2017-11-29 00:43:59 -0200

    Display share name on list

--- src/Share/List.test.js
+++ src/Share/List.test.js
@@ -1,4 +1,15 @@
-import { mapStateToProps } from './List';
+import { List, mapStateToProps } from './List';
+import { shallow } from 'enzyme';
+import React from 'react';
+
+describe('List', () => {
+  it('renders share name', () => {
+    const shares = [{ name: 'Some share' }];
+    const list = shallow(<List shares={shares} />);
+
+    expect(list.text()).toContain('Some share');
+  });
+});
 
 describe('mapStateToProps', () => {
   it('maps shares', () => {
