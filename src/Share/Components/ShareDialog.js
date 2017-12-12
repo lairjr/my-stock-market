d5fc9db57a68b44ea7a8f069ee1b129ec2b98a91
lairjr <lairjr@outlook.com>
2017-12-12 20:57:35 -0200

    Use dialog actions

--- src/Share/Components/ShareDialog.js
+++ src/Share/Components/ShareDialog.js
@@ -3,32 +3,42 @@ import PropTypes from 'prop-types';
 import FlatButton from 'material-ui/FlatButton';
 import MaterialDialog from 'material-ui/Dialog';
 
-const actions = [
-  <FlatButton
-    label="Cancel"
-    onClick={() => {}}
-  />,
-  <FlatButton
-    label="Save"
-    primary
-    keyboardFocused
-    onClick={() => {}}
-  />,
-];
+const ShareDialog = ({ isOpen, onCancel, onSubmit }) => {
+  const actions = [
+    <FlatButton
+      label="Cancel"
+      onClick={onCancel}
+    />,
+    <FlatButton
+      label="Save"
+      primary
+      keyboardFocused
+      onClick={onSubmit}
+    />,
+  ];
 
-const ShareDialog = ({ isOpen }) => (
-  <MaterialDialog
-    title="New share"
-    modal={false}
-    open={isOpen}
-    actions={actions}
-  >
-     Open a Date Picker dialog from within a dialog.
-  </MaterialDialog>
-);
+  return (
+    <MaterialDialog
+      title="New share"
+      modal={false}
+      open={isOpen}
+      actions={actions}
+    >
+       Open a Date Picker dialog from within a dialog.
+    </MaterialDialog>
+  );
+};
+
+ShareDialog.defaultProps = {
+  isOpen: false,
+  onCancel: () => {},
+  onSubmit: () => {},
+};
 
 ShareDialog.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
+  isOpen: PropTypes.bool,
+  onCancel: PropTypes.func,
+  onSubmit: PropTypes.func,
 };
 
 export default ShareDialog;
--- src/Share/Components/ShareDialog.test.js
+++ src/Share/Components/ShareDialog.test.js
@@ -8,4 +8,20 @@ describe('ShareDialog', () => {
 
     expect(dialog.prop('open')).toBe(true);
   });
+
+  it('uses onCancel in the first action', () => {
+    const onCancel = jest.fn();
+    const dialog = shallow(<ShareDialog onCancel={onCancel} />);
+    const firstAction = dialog.prop('actions')[0];
+
+    expect(firstAction.props.onClick).toBe(onCancel);
+  });
+
+  it('uses onSubmit in the second action', () => {
+    const onSubmit = jest.fn();
+    const dialog = shallow(<ShareDialog onSubmit={onSubmit} />);
+    const secondAction = dialog.prop('actions')[1];
+
+    expect(secondAction.props.onClick).toBe(onSubmit);
+  });
 });
