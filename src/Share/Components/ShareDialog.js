3628224045e0790cfdc6f3562a07b6c53eed3d88
lairjr <lairjr@outlook.com>
2017-12-12 19:51:51 -0200

    Add actions to dialog

--- src/Share/Components/ShareDialog.js
+++ src/Share/Components/ShareDialog.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import FlatButton from 'material-ui/FlatButton';
 import MaterialDialog from 'material-ui/Dialog';
+
+const actions = [
+  <FlatButton
+    label="Cancel"
+    onClick={() => {}}
+  />,
+  <FlatButton
+    label="Save"
+    primary
+    keyboardFocused
+    onClick={() => {}}
+  />,
+];
 
 const ShareDialog = ({ isOpen }) => (
   <MaterialDialog
     title="New share"
     modal={false}
     open={isOpen}
+    actions={actions}
   >
      Open a Date Picker dialog from within a dialog.
   </MaterialDialog>
